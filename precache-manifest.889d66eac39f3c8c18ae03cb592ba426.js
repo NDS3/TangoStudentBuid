0235275d796aa6691388a8a4e703722844b5d3c1
NDS <dddddd1997@gmail.com>
2020-10-07 15:05:35 +0300

    init commit

---
+++ precache-manifest.889d66eac39f3c8c18ae03cb592ba426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b5a5bf019083540039267d75f4b4c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "244b44c38c8e4c12c571",
+    "url": "/static/css/main.ec730342.chunk.css"
+  },
+  {
+    "revision": "26c95c1f24911c1b1c83",
+    "url": "/static/js/2.3ac52a1b.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.3ac52a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244b44c38c8e4c12c571",
+    "url": "/static/js/main.3f032d12.chunk.js"
+  },
+  {
+    "revision": "4d4de1f9fd03780d24fe",
+    "url": "/static/js/runtime-main.19e3966e.js"
+  }
+]);
